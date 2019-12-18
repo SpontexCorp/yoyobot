d9373633a066caa691714fd0fad6b4bcb5cddd13
SpontexCorp <50102525+SpontexCorp@users.noreply.github.com>
2019-12-18 18:11:22 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import org.javacord.api.*;
+
+public class Main{
+    public static void main(String[] args) {
+        String token = "nOnE oF yOuR bEesWAx";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        api.setMessageCacheSize(10, 60 * 60);
+
+        api.addListener(new BotListener());
+
+    }
+}
