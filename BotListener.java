35957e345aebc3b98689064e5b11dc1fd784b3d8
SpontexCorp <50102525+SpontexCorp@users.noreply.github.com>
2019-12-18 18:25:22 +0100

    Add files via upload

--- BotListener.java
+++ BotListener.java
@@ -103,56 +103,8 @@ public class BotListener extends Main implements MessageCreateListener {
                             .getChannel()
                             .sendMessage("Ping! " + user.getMentionTag()));
         }
-        if (content.equalsIgnoreCase("y.invite")) {
-            event.getChannel().sendMessage("You can invite the bot by using the following url: https://discordapp.com/oauth2/authorize?client_id=480472135207223309&scope=bot&permissions=8");
-        }
-        if (content.equalsIgnoreCase("reactSmile")) {
-            message.addReaction("😃");
-        }
-        if (content.equalsIgnoreCase("Franklin the")) {
-            event.getChannel().sendMessage(":turtle:");
-        }
-        if (content.equalsIgnoreCase("y.wacky-sent")) {
-            event.getChannel().sendMessage(e.randWackSent());
-        }
-        if (content.equalsIgnoreCase("y.sent")) {
-            event.getChannel().sendMessage(e.randSent());
-        }
-        if (content.equalsIgnoreCase("y.yt")) {
-            event.getChannel().sendMessage(e.randYT());
-        }
         if (content.equalsIgnoreCase("y.help")) {
-            event.getChannel().sendMessage("List of commands coming soon!");
-        }
-        if (content.equalsIgnoreCase("y.art1")) {
-            EmbedBuilder embed = new EmbedBuilder()
-                    .setTitle("Zen")
-                    .setDescription("This piece of abstract art perfectly portrays the idea of zen, despite the chaotic surroundings. No doubt this five minute paint project will be worth a million dollars.")
-                    .setAuthor("Yin4TheWin", null, "https://cdn.discordapp.com/emojis/478590372390633473.png?v=1")
-                    .addField("Estimated Value", "USD 420.69 million")
-                    .addInlineField("Messy Lines and BG (Meaning)", "Sometimes I even confuse myself.")
-                    .addInlineField("Singular Dot (Meaning)", "The embodiment of zen itself, representing human endurance in tough times, yada yada.")
-                    .setColor(Color.BLUE)
-                    .setFooter("Copyrighted by me jk", "https://cdn.discordapp.com/emojis/478590372390633473.png?v=1")
-                    .setImage(new File("C:\\Users\\Franklin\\Desktop\\Zen.png"))
-                    .setThumbnail(new File("C:\\Users\\Franklin\\Desktop\\Zen.png"));
-            event.getChannel().sendMessage(embed);
-        }
-        if (content.length()>6&&content.substring(0, 6).equals("y.ban ")) {
-            event.deleteMessage();
-            event.getChannel().sendMessage("Blacklist updated! To view blacklist use the \"y.list\" command.");
-            blacklist.add(content.substring(6));
-        }
-        if(content.equalsIgnoreCase("I solemnly swear that I'm up to no good"))
-        {
-            String str = "сheese";
-            int codePointAt0 = Character.codePointAt(str, 0);
-            event.getChannel().sendMessage("*cue magic flashes* "+codePointAt0);
-        }
-        if(content.equalsIgnoreCase("Thanks Yin Bot"))
-        {
-            message.addReaction("😃");
-            event.getChannel().sendMessage("Any time!");
+            event.getChannel().sendMessage("Tu crois que c'est du respect ça mon garçon ?");
         }
         for(int i = 0; i <content.length(); i++) {
             if(Character.UnicodeBlock.of(content.charAt(i)).equals(Character.UnicodeBlock.CYRILLIC)) {
--- Main.java
+++ Main.java
@@ -10,5 +10,43 @@ public class Main{
 
         api.addListener(new BotListener());
 
+        String YodaTextPoint = getYodaStringPoint('.');
+        String YodaTextComma = getYodaStringPoint(',');       
+    }
+
+    public String getYodaStringPoint(char charSplit){
+    	String csvFile = "yoda.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = charSplit;
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+
     }
 }
